89674355971400c6fae9c66e324af095701d0e7e
Abiria <abiria.dev@gmail.com>
2024-01-09 18:37:10 +0900

    feat: add example

---
+++ examples/script.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/abiriadev/pia/api"
+)
+
+func main() {
+	res, err := api.GetScript(1720152)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(res)
+}
