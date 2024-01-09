93072ad1a808d9f80510b15089ec8dd767816628
Abiria <abiria.dev@gmail.com>
2024-01-09 17:58:12 +0900

    feat: complete script API

--- api/script.go
+++ api/script.go
@@ -1,9 +1,16 @@
 package api
 
+import "strconv"
+
 type Script struct {
 	Lines []string
 }
 
-func GetScript(id int) Script {
-	// todo
+func GetScript(id int) (Script, error) {
+	s, err := getJson[Script](endPoint + strconv.Itoa(id))
+	if err != nil {
+		return s, err
+	}
+
+	return s, nil
 }
