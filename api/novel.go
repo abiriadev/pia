8ff5b190eacecc38c4c132eebc6f4dac239d775b
Abiria <abiria.dev@gmail.com>
2024-01-15 11:43:53 +0900

    feat: implement novel parse logic

--- api/novel.go
+++ api/novel.go
@@ -1,5 +1,18 @@
 package api
 
+import (
+	"strconv"
+)
+
 type Novel struct {
 	Title string
+}
+
+func GetNovel(id int) (Novel, error) {
+	novel, err := getHtml[Novel](endPointNovel + strconv.Itoa(id))
+	if err != nil {
+		return *new(Novel), err
+	}
+
+	return novel, nil
 }
