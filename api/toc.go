edf127adaf62f5029a0a03635f9819362cdf4cc1
Abiria <abiria.dev@gmail.com>
2024-01-15 15:28:44 +0900

    feat: send form data

--- api/toc.go
+++ api/toc.go
@@ -1,11 +1,18 @@
 package api
 
+import (
+	"net/url"
+	"strconv"
+)
+
 type Toc struct {
 	Chapters []string `goquery:"#episode_list_viewer > table > tr > .ion-bookmark"`
 }
 
 func GetToc(id int) (Toc, error) {
-	toc, err := getHtml[Toc](endPointToc)
+	toc, err := postHtml[Toc](endPointToc, url.Values{
+		"novel_no": []string{strconv.Itoa(id)},
+	})
 	if err != nil {
 		return *new(Toc), err
 	}
