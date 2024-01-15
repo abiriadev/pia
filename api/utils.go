f9e9fafd10de7675a5cf3f6732000556e5766bd2
Abiria <abiria.dev@gmail.com>
2024-01-15 14:47:55 +0900

    refactor: extract ua setting logic

--- api/utils.go
+++ api/utils.go
@@ -8,20 +8,29 @@ import (
 	"astuart.co/goq"
 )
 
+func call(req http.Request) (io.Reader, error) {
+	req.Header.Add("User-Agent", "pia")
+
+	res, err := http.DefaultClient.Do(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Body, nil
+}
+
 func getBody(url string) (io.Reader, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Add("User-Agent", "pia")
-
-	res, err := http.DefaultClient.Do(req)
+	res, err := call(*req)
 	if err != nil {
 		return nil, err
 	}
 
-	return res.Body, nil
+	return res, nil
 }
 
 func getJson[T any](url string) (T, error) {
