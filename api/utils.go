515bce0b51fda964c928b7de223f19eac50565ae
Abiria <abiria.dev@gmail.com>
2024-01-15 11:35:37 +0900

    refactor: let `getJson` to call `getBody`

--- api/utils.go
+++ api/utils.go
@@ -6,29 +6,6 @@ import (
 	"net/http"
 )
 
-func getJson[T any](url string) (T, error) {
-	var body T
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return body, err
-	}
-
-	req.Header.Add("User-Agent", "pia")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return body, err
-	}
-
-	err = json.NewDecoder(res.Body).Decode(&body)
-	if err != nil {
-		return body, err
-	}
-
-	return body, nil
-}
-
 func getBody(url string) (io.Reader, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -43,4 +20,20 @@ func getBody(url string) (io.Reader, error) {
 	}
 
 	return res.Body, nil
+}
+
+func getJson[T any](url string) (T, error) {
+	var body T
+
+	res, err := getBody(url)
+	if err != nil {
+		return body, err
+	}
+
+	err = json.NewDecoder(res).Decode(&body)
+	if err != nil {
+		return body, err
+	}
+
+	return body, nil
 }
