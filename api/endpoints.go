33d2a3298ecfffb263521d6bd824a5c5393c7490
Abiria <abiria.dev@gmail.com>
2024-01-16 15:19:24 +0900

    refactor: extract repeateded part from endpoints

--- api/endpoints.go
+++ api/endpoints.go
@@ -1,8 +1,11 @@
 package api
 
 const (
-	endPointNovel   = "https://novelpia.com/novel/"
-	endPointToc     = "https://novelpia.com/proc/episode_list_viewer"
-	endPointChapter = "https://novelpia.com/viewer/"
-	endPointScript  = "https://novelpia.com/proc/viewer_data/"
+	endPoint = "https://novelpia.com/"
+	proc     = endPoint + "proc/"
+
+	endPointNovel   = endPoint + "novel/"
+	endPointToc     = proc + "episode_list_viewer"
+	endPointChapter = endPoint + "viewer/"
+	endPointScript  = proc + "viewer_data/"
 )
