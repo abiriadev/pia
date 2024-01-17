22d69d854d54696ec3268de5996009cd4102d065
Abiria <abiria.dev@gmail.com>
2024-01-17 13:20:21 +0900

    docs: add example for author comment

---
+++ examples/author-comment.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/abiriadev/pia/api"
+	"github.com/kr/pretty"
+)
+
+func main() {
+	res, err := api.GetAuthorComment(1720152)
+	if err != nil {
+		panic(err)
+	}
+
+	pretty.Println(res.Comment)
+}
