c3b72381cda75ce33c8b07b0cf057bfb897dffae
Abiria <abiria.dev@gmail.com>
2024-03-19 17:17:15 +0900

    feat: define wrapper structs

--- lib.go
+++ lib.go
@@ -1,1 +1,27 @@
 package pia
+
+import "github.com/abiriadev/pia/api"
+
+type PiaClient struct {
+	token string
+}
+
+func NewPiaClient(token string) PiaClient {
+	return PiaClient{token}
+}
+
+type Novel struct {
+	id int
+}
+
+func (n Novel) Content() (string, error) {
+	s, err := api.GetScript(n.id)
+	if err != nil {
+		return "", err
+	}
+	return s.Textify(), nil
+}
+
+func (p *PiaClient) Novel(id int) Novel {
+	return Novel{id}
+}
